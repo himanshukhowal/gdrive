d5d3e748872d52c61bcee9996f3938298af58a06
himanshukhowal <57628508+himanshukhowal@users.noreply.github.com>
2021-07-11 02:32:32 +0530

    Update and rename apps2.js to apps3.js

--- apps3.js
+++ apps3.js
@@ -913,11 +913,11 @@ function file_video(path) {
   <div class="card text-center">
   <div class="text-center">
   <div class="${UI.file_view_alert_class}" id="file_details" role="alert">${obj.name}<br>${size}</div>
-	<div  style="width: 640px; height: 480px;">
+	<div style="width: 640px; height: 480px; position: relative;">
 
-      <iframe src="https://drive.google.com/file/d/`+ obj.id +`/preview"   style="width: 640px; height: 480px;" frameborder="0" scrolling="no" seamless="" allowfullscreen="true"></iframe>
+      <iframe src="https://drive.google.com/file/d/`+ obj.id +`/preview" width="640" height="480" frameborder="0" scrolling="no" seamless="" allowfullscreen="true"></iframe>
 
-      <div style="width: 80px; height: 80px; position: absolute; opacity: 0; right: 0px; top: 0px;">&nbsp;</div>
+      <div style="width: 80px; height: 80px; position: absolute; opacity: 0; right: 0px; top: 0px;"> </div>
 </div>
   </div>
 	${UI.disable_player ? '<style>.plyr{display:none;}</style>' : ''}
